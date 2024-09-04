be47e198ffed24cf19bb8d8fb424151800666208
Luiz Goncalves <51576792+luizhcgoncalves@users.noreply.github.com>
2024-09-04 11:42:03 -0500

    Part 6 complete

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,61 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # Strip all whitespaces from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+            else:
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].replace(' ...more', '')
+
+        # Category and Product Type to lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+
+        # Price without pound symbol and cast to float format
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            adapter[price_key] = float(value)
+
+        # Availability showing only number of books in stock if any
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+
+        if (len(split_string_array) < 2):
+            adapter['availability'] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
+
+        # Number of reviews as integer
+        num_review_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_review_string)
+
+        # Review stars as number
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+
+        stars_text_value = split_stars_array[1].lower()
+        if stars_text_value == 'zero':
+            adapter['stars'] = 0
+        elif stars_text_value == 'one':
+            adapter['stars'] = 1
+        elif stars_text_value == 'two':
+            adapter['stars'] = 2
+        elif stars_text_value == 'three':
+            adapter['stars'] = 3
+        elif stars_text_value == 'four':
+            adapter['stars'] = 4
+        elif stars_text_value == 'five':
+            adapter['stars'] = 5
+
         return item
