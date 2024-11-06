2fe02bfa77943e431d05233a33d4030e528545bf
Luiz Gonçalves <51576792+luizhcgoncalves@users.noreply.github.com>
2024-11-05 19:53:51 -0600

    Delete Automate with Python directory
    Not my code

--- Automate with Python/Project 1/web_table.py
+++
@@ -1,5 +0,0 @@
-# pip install pandas
-import pandas as pd
-
-episodes = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
-# return the list of all tables in the website
